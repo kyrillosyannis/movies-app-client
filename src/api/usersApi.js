ded537d065bfa471251d4b10fdb801b6cda8f844
yanniskyrillos <yannis.kyrillos@athenarc.gr>
2025-06-25 17:07:24 +0300

    set api url as env var

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,7 +1,10 @@
 import {handleError, handleResponse, headerAttributes} from "./apiUtils";
+import { config } from '../envs/env';
+
+const BASE_URL = config.API_URL;
 
 export const registerUser = (registrationRequest) => {
-    return fetch(`/register`, {
+    return fetch(`${BASE_URL}/register`, {
         method: 'POST',
         headers: headerAttributes,
         body: JSON.stringify(registrationRequest)
@@ -11,7 +14,7 @@ export const registerUser = (registrationRequest) => {
 };
 
 export const authenticate = authenticationRequest => {
-    return fetch(`/authenticate`, {
+    return fetch(`${BASE_URL}/authenticate`, {
         method: 'POST',
         headers: headerAttributes,
         body: JSON.stringify(authenticationRequest)
